74b329d3a5716ae6a0d6445ed51e1696eb399bc2
Eloi Sans <e.sans@uib.eu>
2024-05-21 16:52:53 +0200

    initial commit

---
+++ conftest.py
@@ -0,0 +1,44 @@
+"""
+Shared PyTest configuration.
+
+In order to run the examples, we need to run the Pact broker. In order to avoid
+having to run the Pact broker manually, or repeating the same code in each
+example, we define a PyTest fixture to run the Pact broker.
+
+We also define a `pact_dir` fixture to define the directory where the generated
+Pact files will be stored. You are encouraged to have a look at these files
+after the examples have been run.
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Generator
+
+import pytest
+from yarl import URL
+
+EXAMPLE_DIR = Path(__file__).parent.resolve()
+
+
+@pytest.fixture(scope="session")
+def broker(request: pytest.FixtureRequest) -> Generator[URL, Any, None]:
+    """
+    Fixture to run the Pact broker.
+
+    This inspects whether the `--broker-url` option has been given. If it has,
+    it is assumed that the broker is already running and simply returns the
+    given URL.
+
+    Otherwise, the Pact broker is started in a container. The URL of the
+    containerised broker is then returned.
+    """
+
+    yield URL("http://pactbroker:pactbroker@localhost:9292")
+    return
+
+
+@pytest.fixture(scope="session")
+def pact_dir() -> Path:
+    """Fixture for the Pact directory."""
+    return EXAMPLE_DIR / "pacts"
---
+++ src/fastapi.py
@@ -0,0 +1,54 @@
+"""
+FastAPI provider example.
+
+This modules defines a simple
+[provider](https://docs.pact.io/getting_started/terminology#service-provider)
+which will be tested with Pact in the [provider
+test][examples.tests.test_01_provider_fastapi]. As Pact is a consumer-driven
+framework, the consumer defines the contract which the provider must then
+satisfy.
+
+The provider is the application which receives requests from another service
+(the consumer) and returns a response. In this example, we have a simple
+endpoint which returns a user's information from a (fake) database.
+
+Note that the code in this module is agnostic of Pact. The `pact-python`
+dependency only appears in the tests. This is because the consumer is not
+concerned with Pact, only the tests are.
+"""
+
+from __future__ import annotations
+
+from typing import Any, Dict
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+"""
+As this is a simple example, we'll use a simple dict to represent a database.
+This would be replaced with a real database in a real application.
+
+When testing the provider in a real application, the calls to the database would
+be mocked out to avoid the need for a real database. An example of this can be
+found in the [test suite][examples.tests.test_01_provider_fastapi].
+"""
+FAKE_DB: Dict[int, Dict[str, Any]] = {}
+
+
+@app.get("/users/{uid}")
+async def get_user_by_id(uid: int) -> JSONResponse:
+    """
+    Fetch a user by their ID.
+
+    Args:
+        uid: The ID of the user to fetch
+
+    Returns:
+        The user data if found, HTTP 404 if not
+    """
+    user = FAKE_DB.get(uid)
+    if not user:
+        return JSONResponse(status_code=404, content={"error": "User not found"})
+    return JSONResponse(status_code=200, content=user)
